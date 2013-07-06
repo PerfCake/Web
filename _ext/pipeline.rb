a465c04b9be3d95440c05018b11ef5b4fde8c380
Martin Večeřa <mvecera@redhat.com>
2013-07-06 23:05:44 +0200

    switched to awestruct

---
+++ _ext/pipeline.rb
@@ -0,0 +1,9 @@
+require 'bootstrap-sass'
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  # extension Awestruct::Extensions::Sitemap.new
+end
